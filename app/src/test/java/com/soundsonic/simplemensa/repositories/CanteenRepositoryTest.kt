436790a1a954f4cb7f8f7f1a56578b9d86beff43
Kien Dang Tran <Kien.Dang_Tran@mailbox.tu-dresden.de>
2019-12-06 21:01:59 +0100

    Add CanteenRepositoryTest

---
+++ app/src/test/java/com/soundsonic/simplemensa/repositories/CanteenRepositoryTest.kt
@@ -0,0 +1,38 @@
+package com.soundsonic.simplemensa.repositories
+
+import com.soundsonic.simplemensa.data.model.Canteen
+import com.soundsonic.simplemensa.data.repositories.CanteenRepository
+import com.soundsonic.simplemensa.data.repositories.CanteenRepositoryImpl
+import com.soundsonic.simplemensa.util.provideApi
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class CanteenRepositoryTest {
+
+    private val repo: CanteenRepository = CanteenRepositoryImpl(provideApi())
+
+    @Test
+    fun `fetch canteens`() {
+        runBlocking {
+            assertTrue(repo.getCanteens().isNotEmpty())
+        }
+    }
+
+    @Test
+    fun `find canteen`() {
+        val canteen = Canteen(
+            id = 4,
+            name = "Alte Mensa",
+            city = "Dresden",
+            address = "Mommsenstr. 13, 01069 Dresden",
+            coordinates = listOf(51.02696733929933, 13.726491630077364),
+            url = "https://www.studentenwerk-dresden.de/mensen/details-alte-mensa.html",
+            menu = "https://www.studentenwerk-dresden.de/mensen/speiseplan/alte-mensa.html"
+        )
+
+        runBlocking {
+            assertTrue(repo.getCanteens().contains(canteen))
+        }
+    }
+}
---
+++ app/src/test/java/com/soundsonic/simplemensa/util/DependencyProvider.kt
@@ -0,0 +1,13 @@
+package com.soundsonic.simplemensa.util
+
+import com.soundsonic.simplemensa.data.api.OpenMensaApi
+import com.soundsonic.simplemensa.util.Constants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+fun provideApi(): OpenMensaApi =
+    Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create(OpenMensaApi::class.java)
