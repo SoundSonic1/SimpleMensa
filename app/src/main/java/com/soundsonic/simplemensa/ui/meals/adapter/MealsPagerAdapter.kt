3faf3ba0564812df16054644a619ab7647e62c28
Kien Dang Tran <Kien.Dang_Tran@mailbox.tu-dresden.de>
2019-12-31 12:09:46 +0100

    Change date format in viewpager

--- app/src/main/java/com/soundsonic/simplemensa/ui/meals/adapter/MealsPagerAdapter.kt
+++ app/src/main/java/com/soundsonic/simplemensa/ui/meals/adapter/MealsPagerAdapter.kt
@@ -7,31 +7,32 @@ import com.soundsonic.simplemensa.data.model.Canteen
 import com.soundsonic.simplemensa.ui.meals.fragment.MealsFragment
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Named
 
 class MealsPagerAdapter @Inject constructor(
     @Named("childFragmentManager") fm: FragmentManager,
-    simpleDateFormat: SimpleDateFormat,
+    private val dateFormatOpenMensa: SimpleDateFormat,
     @Named("CanteenTab") private val canteen: Canteen
 ) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     private val calendar = Calendar.getInstance()
+    private val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.GERMANY)
 
     private val dates = listOf<String>(
-        simpleDateFormat.format(calendar.time),
-        simpleDateFormat.format(calendar.apply { add(Calendar.DAY_OF_YEAR, 1) }.time),
-        simpleDateFormat.format(calendar.apply { add(Calendar.DAY_OF_YEAR, 1) }.time)
+        dateFormatOpenMensa.format(calendar.time),
+        dateFormatOpenMensa.format(calendar.apply { add(Calendar.DAY_OF_YEAR, 1) }.time),
+        dateFormatOpenMensa.format(calendar.apply { add(Calendar.DAY_OF_YEAR, 1) }.time)
     )
 
-    override fun getCount(): Int = 3
+    override fun getCount(): Int = dates.size
 
     override fun getItem(position: Int): Fragment =
-        when (position) {
-            0 -> MealsFragment.newInstance(canteen, dates[position])
-            1 -> MealsFragment.newInstance(canteen, dates[position])
-            else -> MealsFragment.newInstance(canteen, dates[position])
-        }
+        MealsFragment.newInstance(canteen, dates[position])
 
-    override fun getPageTitle(position: Int): CharSequence? = dates[position]
+    override fun getPageTitle(position: Int): CharSequence? {
+        val date = dateFormatOpenMensa.parse(dates[position])!!
+        return sdf.format(date)
+    }
 }
