a4fbdb46688135f8ef33b9ac465705ffbc3711fd
Kien Dang <kiendangtran0@gmail.com>
2021-09-25 21:04:40 +0200

    Fix tests

--- app/src/test/java/com/soundsonic/simplemensa/util/DependencyProvider.kt
+++ app/src/test/java/com/soundsonic/simplemensa/util/DependencyProvider.kt
@@ -5,7 +5,6 @@ import com.soundsonic.simplemensa.data.database.CanteenDao
 import com.soundsonic.simplemensa.data.model.Canteen
 import com.soundsonic.simplemensa.data.repositories.CanteenRepository
 import com.soundsonic.simplemensa.data.repositories.CanteenRepositoryImpl
-import com.soundsonic.simplemensa.util.Constants.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -21,7 +20,7 @@ fun provideMockCanteenDao(): CanteenDao = object : CanteenDao {
 
 fun provideApi(): OpenMensaApi =
     Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl("https://api.studentenwerk-dresden.de/openmensa/v2/")
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
         .create(OpenMensaApi::class.java)
--- app/src/test/java/com/soundsonic/simplemensa/viewmodel/CanteenViewModelTest.kt
+++ app/src/test/java/com/soundsonic/simplemensa/viewmodel/CanteenViewModelTest.kt
@@ -1,13 +1,17 @@
 package com.soundsonic.simplemensa.viewmodel
 
+import com.soundsonic.simplemensa.data.model.Canteen
+import com.soundsonic.simplemensa.data.repositories.CanteenRepository
+import com.soundsonic.simplemensa.data.repositories.UserRepository
 import com.soundsonic.simplemensa.ui.home.CanteenViewModel
 import com.soundsonic.simplemensa.util.InstantExecutorExtension
 import com.soundsonic.simplemensa.util.getOrAwaitValue
-import com.soundsonic.simplemensa.util.provideCanteenRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterEach
@@ -15,13 +19,22 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
 
 @ExperimentalCoroutinesApi
 @ObsoleteCoroutinesApi
-@ExtendWith(InstantExecutorExtension::class)
+@ExtendWith(InstantExecutorExtension::class, MockitoExtension::class)
 class CanteenViewModelTest {
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Mock
+    private lateinit var canteenRepository: CanteenRepository
+
+    @Mock
+    private lateinit var userRepository: UserRepository
 
     @BeforeEach
     fun beforeEach() = Dispatchers.setMain(mainThreadSurrogate)
@@ -34,7 +47,26 @@ class CanteenViewModelTest {
 
     @Test
     fun `load canteens on creation`() {
-        val viewModel = CanteenViewModel(provideCanteenRepo())
-        assertTrue(viewModel.canteens.getOrAwaitValue().isNotEmpty())
+
+        runBlocking {
+            `when`(userRepository.userProfile).thenReturn(MutableStateFlow(null))
+
+            `when`(canteenRepository.getCanteens()).thenReturn(
+                listOf(
+                    Canteen(
+                        id = 1,
+                        name = "",
+                        city = "",
+                        address = "",
+                        coordinates = listOf(),
+                        url = "",
+                        menu = ""
+                    )
+                )
+            )
+
+            val viewModel = CanteenViewModel(userRepository, canteenRepository)
+            assertTrue(viewModel.canteens.getOrAwaitValue().isNotEmpty())
+        }
     }
 }
