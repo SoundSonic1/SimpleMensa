aecbd03d95ffb10178805b8bd3bd60fc20d119e5
Kien Dang <kiendangtran0@gmail.com>
2021-09-27 11:36:33 +0200

    Add ui test coverage

--- app/src/androidTest/java/com/soundsonic/simplemensa/MainActivityTest.kt
+++ app/src/androidTest/java/com/soundsonic/simplemensa/MainActivityTest.kt
@@ -1,9 +1,14 @@
 package com.soundsonic.simplemensa
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.soundsonic.simplemensa.di.MockNetworkModule.HOST
@@ -13,6 +18,7 @@ import com.soundsonic.simplemensa.util.atPosition
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -41,10 +47,26 @@ class MainActivityTest {
     }
 
     @Test
-    fun showCanteens() {
+    fun showCanteenAndNavigateToMeal() {
         mockWebServer.enqueueResponse("canteens.json")
         ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.canteen_recycler_view))
             .check(matches(atPosition(0, hasDescendant(withText("Mensa Reichenbachstraße")))))
+        mockWebServer.enqueueResponse("canteen_meals.json")
+        onView(withId(R.id.canteen_recycler_view))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(allOf(withId(R.id.meal_recycler_view), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+            .check(
+                matches(
+                    atPosition(
+                        0,
+                        hasDescendant(
+                            withText("Chili sin carne (I, L) mit Basmati Reis, dazu Salat (J)")
+                        )
+                    )
+                )
+            )
+        onView(allOf(withId(R.id.meal_recycler_view), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
     }
 }
